2bebec90b427d5d75672cf6764e4fed59f47acc4
pvillela <pvillela@gmail.com>
2024-07-14 00:07:20 -0400

    WIP 2024-07-14 00:06 -- substantial improvement to core_error with introduction of contextual message table and locale.

--- examples/core_error_example.rs
+++ examples/core_error_example.rs
@@ -1,73 +1,86 @@
-use std::{collections::HashMap, error::Error as StdError, sync::Arc};
+use std::{
+    collections::HashMap,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
-use foa::{CoreError, NoDebug};
+use foa::{CoreError, ErrorDisplayMap, Locale};
 use once_cell::sync::Lazy;
 
 static ERROR_DISPLAY_MAP: Lazy<HashMap<&'static str, &'static str>> = Lazy::new(|| {
     HashMap::from([
-        (ERROR_KIND_0, "no args"),
-        (ERROR_KIND_1, "one arg is {} and that's it"),
-        (ERROR_KIND_2, "two args are {} and {} and that's it"),
+        ("err_kind_0-en-ca", "no args"),
+        ("err_kind_1-en-ca", "one arg is {} and that's it"),
+        ("err_kind_2-en-ca", "two args are {} and {} and that's it"),
+        ("err_kind_0-pt-br", "nenhum parâmetro"),
+        ("err_kind_1-pt-br", "um parâmetro {} e é só"),
+        ("err_kind_2-pt-br", "dois parâmetros {} e {} e nada mais"),
     ])
 });
-
-fn new_core_error(
-    kind: &'static str,
-    args: Vec<String>,
-    source: Option<Arc<dyn StdError>>,
-) -> CoreError {
-    CoreError {
-        kind,
-        args,
-        source,
-        display_map: NoDebug(&ERROR_DISPLAY_MAP),
-    }
-}
 
 const ERROR_KIND_0: &str = "err_kind_0";
 const ERROR_KIND_1: &str = "err_kind_1";
 const ERROR_KIND_2: &str = "err_kind_2";
 
+#[derive(Debug, Clone)]
+struct Ctx;
+
+impl ErrorDisplayMap for Ctx {
+    fn display_map<'a>() -> &'a HashMap<&'a str, &'a str> {
+        &ERROR_DISPLAY_MAP
+    }
+}
+
+static LOCALE_SELECTOR: AtomicUsize = AtomicUsize::new(0);
+const LOCALES: [&str; 3] = ["en-ca", "pt-br", "es-es"];
+
+impl Locale for Ctx {
+    fn locale<'a>() -> &'a str {
+        LOCALES[LOCALE_SELECTOR.load(Ordering::Relaxed)]
+    }
+}
+
+type MyCoreError = CoreError<Ctx>;
+
 fn main() {
-    let err0 = new_core_error(&ERROR_KIND_0, vec![], None);
-    let err1 = new_core_error(
-        &ERROR_KIND_1,
-        vec!["arg1".to_owned()],
-        Some(Arc::new(err0.clone())),
-    );
-    let err2 = new_core_error(
-        &ERROR_KIND_2,
-        vec!["param1".to_owned(), "param2".to_owned()],
-        Some(Arc::new(err1.clone())),
-    );
-    let err2_lo = new_core_error(
-        &ERROR_KIND_2,
-        vec!["xxx".to_owned()],
-        Some(Arc::new(err1.clone())),
-    );
-    let err2_hi = new_core_error(
-        &ERROR_KIND_2,
-        vec!["x".to_owned(), "y".to_owned(), "z".to_owned()],
-        Some(Arc::new(err1.clone())),
-    );
+    for i in 0..3 {
+        LOCALE_SELECTOR.store(i, Ordering::Relaxed);
+        println!("***** Locale={}", LOCALES[i]);
+        println!();
 
-    println!("err0={err0:?}");
-    println!("err0 msg={err0}");
-    println!();
+        let err0 = MyCoreError::new(&ERROR_KIND_0, vec![]);
+        let err1 =
+            MyCoreError::new_with_source(&ERROR_KIND_1, vec!["arg1".to_owned()], err0.clone());
+        let err2 = MyCoreError::new_with_source(
+            &ERROR_KIND_2,
+            vec!["param1".to_owned(), "param2".to_owned()],
+            err1.clone(),
+        );
+        let err2_lo =
+            MyCoreError::new_with_source(&ERROR_KIND_2, vec!["xxx".to_owned()], err1.clone());
+        let err2_hi = MyCoreError::new_with_source(
+            &ERROR_KIND_2,
+            vec!["x".to_owned(), "y".to_owned(), "z".to_owned()],
+            err1.clone(),
+        );
 
-    println!("err1={err1:?}");
-    println!("err1 msg={err1}");
-    println!();
+        println!("err0={err0:?}");
+        println!("err0 msg={err0}");
+        println!();
 
-    println!("err2={err2:?}");
-    println!("err2 msg={err2}");
-    println!();
+        println!("err1={err1:?}");
+        println!("err1 msg={err1}");
+        println!();
 
-    println!("err2_lo={err2_lo:?}");
-    println!("err2_lo msg={err2_lo}");
-    println!();
+        println!("err2={err2:?}");
+        println!("err2 msg={err2}");
+        println!();
 
-    println!("err2_hi={err2_hi:?}");
-    println!("err2_hi msg={err2_hi}");
-    println!();
+        println!("err2_lo={err2_lo:?}");
+        println!("err2_lo msg={err2_lo}");
+        println!();
+
+        println!("err2_hi={err2_hi:?}");
+        println!("err2_hi msg={err2_hi}");
+        println!();
+    }
 }
--- src/core_error.rs
+++ src/core_error.rs
@@ -1,7 +1,8 @@
-use std::{collections::HashMap, error::Error as StdError, fmt::Debug, sync::Arc};
-use thiserror::Error;
-
 use crate::{display_interpolated, NoDebug};
+use std::{
+    collections::HashMap, error::Error as StdError, fmt::Debug, marker::PhantomData, sync::Arc,
+};
+use thiserror::Error;
 
 fn display_msg(display_map: &HashMap<&str, &str>, display_key: &str, args: &Vec<String>) -> String {
     let raw_msg = display_map.get(display_key);
@@ -11,11 +12,53 @@ fn display_msg(display_map: &HashMap<&str, &str>, display_key: &str, args: &Vec<
     display_interpolated(raw_msg, args)
 }
 
+pub trait ErrorDisplayMap {
+    fn display_map<'a>() -> &'a HashMap<&'a str, &'a str>;
+}
+
+pub trait Locale {
+    fn locale<'a>() -> &'a str;
+}
+
 #[derive(Error, Debug, Clone)]
-#[error("{}", display_msg(display_map, kind, args))]
-pub struct CoreError {
+#[error("{}", display_msg(CTX::display_map(), &self.display_key(), args))]
+pub struct CoreError<CTX>
+where
+    CTX: ErrorDisplayMap + Locale,
+{
     pub kind: &'static str,
-    pub display_map: NoDebug<&'static HashMap<&'static str, &'static str>>,
     pub args: Vec<String>,
     pub source: Option<Arc<dyn StdError>>,
+    _ctx: NoDebug<PhantomData<CTX>>,
+}
+
+impl<CTX> CoreError<CTX>
+where
+    CTX: ErrorDisplayMap + Locale,
+{
+    pub fn new(kind: &'static str, args: Vec<String>) -> Self {
+        CoreError {
+            kind,
+            args,
+            source: None,
+            _ctx: NoDebug(PhantomData),
+        }
+    }
+
+    pub fn new_with_source(
+        kind: &'static str,
+        args: Vec<String>,
+        source: impl StdError + 'static,
+    ) -> Self {
+        CoreError {
+            kind,
+            args,
+            source: Some(Arc::new(source)),
+            _ctx: NoDebug(PhantomData),
+        }
+    }
+
+    fn display_key(&self) -> String {
+        self.kind.to_owned() + "-" + CTX::locale()
+    }
 }
