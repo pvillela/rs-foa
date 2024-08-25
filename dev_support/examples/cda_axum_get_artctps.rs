8ba91e0a7c73bcde7effb1caeb9d82d1251bf78f
pvillela <pvillela@gmail.com>
2024-08-25 11:12:27 -0400

    Updated cda_axum_get_artctps to return Result<_, FoaError<CTX>>.

--- dev_support/examples/cda_axum_get_artctps.rs
+++ dev_support/examples/cda_axum_get_artctps.rs
@@ -1,59 +1,15 @@
 use axum::{extract::State, http::StatusCode, Json, Router};
 use dev_support::artctps::{Ctx, FooIn, FooOut, FooSfl, FooSflI};
-use serde::{Deserialize, Serialize};
-use sqlx::{PgPool, Row};
-
-// #[derive(Deserialize)]
-// struct CreateUser {
-//     name: String,
-//     email: String,
-// }
-
-// #[derive(Serialize)]
-// struct User {
-//     id: i32,
-//     name: String,
-//     email: String,
-// }
+use foa::error::FoaError;
+use sqlx::PgPool;
 
 async fn foo_sfl_hdlr(
     State(pool): State<PgPool>,
     Json(input): Json<FooIn>,
-) -> Result<(StatusCode, Json<FooOut>), StatusCode> {
-    let mut tx = pool
-        .begin()
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    // let foo_out = FooOut { res: "done".into() };
-    let foo_out = FooSflI::<Ctx>::foo_sfl(input, &mut tx)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    // let row =
-    //     sqlx::query("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id, name, email")
-    //         .bind(&input.name)
-    //         .bind(&input.email)
-    //         .fetch_one(&mut *tx)
-    //         .await
-    //         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    // let user = User {
-    //     id: row
-    //         .try_get("id")
-    //         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-    //     name: row
-    //         .try_get("name")
-    //         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-    //     email: row
-    //         .try_get("email")
-    //         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-    // };
-
-    tx.commit()
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
+) -> Result<(StatusCode, Json<FooOut>), FoaError<Ctx>> {
+    let mut tx = pool.begin().await?;
+    let foo_out = FooSflI::<Ctx>::foo_sfl(input, &mut tx).await?;
+    tx.commit().await?;
     Ok((StatusCode::CREATED, Json(foo_out)))
 }
 
