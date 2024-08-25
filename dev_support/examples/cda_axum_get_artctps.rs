2677d9cdce6005ce738ce8504f45fb764cf4c9cd
pvillela <pvillela@gmail.com>
2024-08-25 12:33:54 -0400

    Modified cda_axum_get_artctps to not use axum::extract::State.

--- dev_support/examples/cda_axum_get_artctps.rs
+++ dev_support/examples/cda_axum_get_artctps.rs
@@ -1,13 +1,15 @@
-use axum::{extract::State, http::StatusCode, Json, Router};
+use axum::{http::StatusCode, Json, Router};
 use dev_support::artctps::{Ctx, FooIn, FooOut, FooSfl, FooSflI};
-use foa::error::FoaError;
-use sqlx::PgPool;
+use foa::{
+    db::sqlx::pg::{Db, Itself},
+    error::FoaError,
+};
 
 async fn foo_sfl_hdlr(
-    State(pool): State<PgPool>,
     Json(input): Json<FooIn>,
 ) -> Result<(StatusCode, Json<FooOut>), FoaError<Ctx>> {
-    let mut tx = pool.begin().await?;
+    let ctx = Ctx::itself();
+    let mut tx = ctx.pool_tx().await?;
     let foo_out = FooSflI::<Ctx>::foo_sfl(input, &mut tx).await?;
     tx.commit().await?;
     Ok((StatusCode::CREATED, Json(foo_out)))
@@ -15,16 +17,11 @@ async fn foo_sfl_hdlr(
 
 #[tokio::main]
 async fn main() {
-    let pool = PgPool::connect("postgres://username:password@localhost/database")
-        .await
-        .expect("Failed to connect to Postgres");
-
-    let app = Router::new()
-        .route("/users", axum::routing::post(foo_sfl_hdlr))
-        .with_state(pool);
+    let app = Router::new().route("/users", axum::routing::post(foo_sfl_hdlr));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
         .unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }
